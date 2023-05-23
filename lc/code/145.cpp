2b09207e7d6469f5a1e5a3506b4c8d8d3399175f
Huiting Hou <houhuiting@stu.pku.edu.cn>
2023-05-23 18:36:57 +0800

    lc 13day

---
+++ lc/code/145.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <unordered_map>
+using namespace std;
+
+struct TreeNode {     
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    void traversal(TreeNode* node, vector<int>& vec) {
+        if (node==nullptr)
+            return;
+        traversal(node->left, vec); // 左
+        traversal(node->right, vec); // 右
+        vec.push_back(node->val); // 中
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        traversal(root, res);
+        return res;
+    }
+};
